9eb3c7e3df382030646c39c29ada7309a07e536d
Onwunzo amara <onwunzoamara@yahoo.com>
2022-04-08 08:52:43 +0000

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 
-	write(2, ch, sizeof(ch));
 	return (1);
 }
 
