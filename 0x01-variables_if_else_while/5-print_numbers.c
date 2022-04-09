3e928cdfd16ddb890e03e862198bd851436b1ad8
Onwunzo amara <onwunzoamara@yahoo.com>
2022-04-09 19:35:28 +0000

    happy

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main 
+ * main - Entry point
  *
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char n;
 
 	for (n = '0'; n <= '9'; n++)
-	{		
+	{
 	putchar(n);
 	}
 
