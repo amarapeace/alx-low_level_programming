942bd8b5658d514ae3c68110dbb45d67ba4b0092
Onwunzo amara <onwunzoamara@yahoo.com>
2022-04-12 21:13:08 +0000

    mandatory

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ *
+ * Return: 1 if the number is greater than zero,
+ * 	0 if the number is zero
+ * 	-1 if the number is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > o)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute vvalue of an integer
+ *
+ * Return: The absolute value
+ */
+int _abs(int n)
+
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
