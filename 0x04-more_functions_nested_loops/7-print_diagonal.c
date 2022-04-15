695eddff79cdb70d86bd143df6068488e3787e1b
Onwunzo amara <onwunzoamara@yahoo.com>
2022-04-15 10:25:22 +0000

    k

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,24 +6,18 @@
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-	int i, j;
+	int i = 0,  j;
 
+	if (n > 0)
+	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-					_putchar('\n');
-			}
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
 		}
 	}
+	else
+		_putchar('\n');
 }
