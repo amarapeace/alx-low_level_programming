6d1dc24fe2fecb29efa0483be5050d5d8f2f2da3
Onwunzo amara <onwunzoamara@yahoo.com>
2022-04-15 13:47:36 +0000

    k

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,6 +7,25 @@
  */
 int main(void)
 {
+	int i, e;
 
+	i = 48
+	e = 4;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
 	return (0);
 }
