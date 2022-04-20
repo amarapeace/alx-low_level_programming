0a4ebf02d3af28a89e2e8cf40b1d9afca3634c0e
Onwunzo amara <onwunzoamara@yahoo.com>
2022-04-20 19:58:27 +0000

    corrction

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,4 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif
+#endif /* _MAIN_H_ */
