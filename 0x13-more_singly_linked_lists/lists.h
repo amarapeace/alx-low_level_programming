11a03fe70531f3ef125e866faeb9e2ae4048cf41
Onwunzo amara <onwunzoamara@yahoo.com>
2022-05-27 13:37:15 +0000

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,11 +1,7 @@
-#ifndef LISTS_H
-#define LISTS_H
-int_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
-#endif#include <stdlib.h>
-
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -36,4 +32,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* _LISTS_H_ */
